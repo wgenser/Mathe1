8b29986833c1572e149a363860224de901836af0
David_GENSER <wgenser.wgenser@gmail.com>
2016-06-13 14:42:53 +0200

    plus rechnen

--- app/src/main/java/com/example/david_genser/mathe1/MainActivity.java
+++ app/src/main/java/com/example/david_genser/mathe1/MainActivity.java
@@ -14,7 +14,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int Zahl1, Zahl2;
+    private int Zahl1, Zahl2; // ist privat, aber gilt innerhalb der class
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +24,9 @@ public class MainActivity extends AppCompatActivity {
         //TextView-Variablen erzeugen
         final TextView Anzeige1 =
                 (TextView) findViewById(R.id.textView);
-        final TextView Anzeige2 =
+        final TextView Anzeige2 =  // linke Zufallszahl
                 (TextView) findViewById(R.id.textView2);
-        final TextView Anzeige3 =
+        final TextView Anzeige3 =  // rechte Zufallszahl
                 (TextView) findViewById(R.id.textView3);
 
         //Button-Variablen erzeugen
@@ -34,8 +34,28 @@ public class MainActivity extends AppCompatActivity {
         Button SubButton = (Button) findViewById(R.id.button2);
         Button MulButton = (Button) findViewById(R.id.button3);
         Button DivButton = (Button) findViewById(R.id.button4);
-        Button NeuButton = (Button) findViewById(R.id.button5);
+        Button NeuButton = (Button) findViewById(R.id.button5); // Neue Zufallszahlen erzeugen
 
+        // Wenn Button gedrückt, dann zwei neue zuf.zahlen
+        NeuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random Zufall =  new Random();
+                Zahl1 = Zufall.nextInt(100)+1;
+                Zahl2 = Zufall.nextInt(100)+1;
+                Anzeige1.setText(R.string.Rechenart); // Ausgabe Rechenart?
+                Anzeige2.setText(String.valueOf(Zahl1));
+                Anzeige3.setText(String.valueOf(Zahl2));
+            }
+        });
+
+        AddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int Ergebnis = Zahl1 + Zahl2;
+                Anzeige1.setText(String.valueOf(Ergebnis));
+            }
+        });
 
     }
 
